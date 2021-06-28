ee17c08a30a4f2a795294673dbf85e4548a22af8
vinson <dagger1245653@gmail.com>
2021-06-27 17:04:37 -0700

    Created a function that will console.log all possible wins

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const players = (letter) => {
                 if (getDivs[div.id - 1].textContent == '') {
                     getDivs[div.id - 1].textContent = 'x';
                     x++;
+                    checkWinner();
                 } else if (getDivs[div.id - 1].textContent != '') {
                     console.log('It\'s already occupied!');
                 }
@@ -24,6 +25,7 @@ const players = (letter) => {
                 if (getDivs[div.id - 1].textContent == '') {
                     getDivs[div.id - 1].textContent = 'o';
                     o++;
+                    checkWinner();
                 } else if (getDivs[div.id - 1].textContent != '') {
                     console.log('It\'s already occupied!');
                 }
@@ -31,21 +33,42 @@ const players = (letter) => {
                 if (getDivs[div.id - 1].textContent == '') {
                     getDivs[div.id - 1].textContent = 'x';
                     x++;
-                    console.log(x);
-                    console.log(o);
+                    checkWinner();
                 } else if (getDivs[div.id - 1].textContent != '') {
                     console.log('It\'s already occupied!');
                 }
             }
         })
     });
+    function checkWinner() {
+        //vertical Wins
+        if (getDivs[9 - 1].textContent == 'x' && getDivs[6 -1].textContent == 'x' && getDivs[3 - 1].textContent == 'x') {
+            console.log('Right Vertical Win');
+        } else if (getDivs[2 -1].textContent == 'x' && getDivs[5 -1].textContent == 'x' && getDivs[8-1].textContent == 'x') {
+            console.log('Middle Vertical Win');
+        } else if (getDivs[1 - 1].textContent == 'x' && getDivs[4 - 1].textContent == 'x' && getDivs[7 - 1].textContent == 'x') {
+            console.log('Left Vertical Win');
+        //Horizontal Wins
+        } else if (getDivs[1 - 1].textContent == 'x' && getDivs[2 - 1].textContent == 'x' && getDivs[3 - 1].textContent == 'x') {
+            console.log('Upper Horizontal Win');
+        } else if (getDivs[4 - 1].textContent == 'x' && getDivs[5 - 1].textContent == 'x' && getDivs[6 - 1].textContent == 'x') {
+            console.log('Middle Horizontal Win');
+        } else if (getDivs[7 - 1].textContent == 'x' && getDivs[8 - 1].textContent == 'x' && getDivs[9 - 1].textContent == 'x') {
+            console.log('Lower Horizontal Win');
+        //Diagonal Wins
+        } else if (getDivs[1 - 1].textContent == 'x' && getDivs[5 - 1].textContent == 'x' && getDivs[9 - 1].textContent == 'x') {
+            console.log('Upper Left to Bottom Right Diagonal Win');
+        } else if (getDivs[7 - 1].textContent == 'x' && getDivs[5 - 1].textContent == 'x' && getDivs[3 - 1].textContent == 'x') {
+            console.log('Bottom Left to Upper Right Diagonal Win');
+        }
+    }
 }
 
 const objectFlow = (() => {
     const getDivs = document.querySelectorAll('.gameboard-display');
-    if (getDivs[9 - 1] == 'x' && getDivs[6 -1] == 'x' && getDivs[3 - 1] == 'x') [
+    if (getDivs[9 - 1] == 'x' && getDivs[6 -1] == 'x' && getDivs[3 - 1] == 'x') {
         console.log('Win')
-    ]
+    }
 })();
 
 const protagonist = players('o');
